07bb27075cd8ab33dd37d7bdab4546765c8cf80b
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 03:58:04 -0700

    Add files via upload
    Q 19 Node.js of 45 Assignments

---
+++ Q19.js
@@ -0,0 +1,8 @@
+"use strict";
+// // 19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message 
+// indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Define an array of place to visit
+let placesToVisit = ["Makkah", "Tokyo", "Paris", "New York", "Thailand"];
+//Print the number of dinner guests
+console.log(`Number of dinner guests: ${placesToVisit.length}`);
